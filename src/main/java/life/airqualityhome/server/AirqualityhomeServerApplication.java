4c84434e77af3ee0dd7ac096dd4bde600a25386f
harald <harald@test.de>
2024-05-24 00:26:54 +0200

    Initial commit

---
+++ src/main/java/life/airqualityhome/server/AirqualityhomeServerApplication.java
@@ -0,0 +1,13 @@
+package life.airqualityhome.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirqualityhomeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirqualityhomeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/life/airqualityhome/server/AirqualityhomeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package life.airqualityhome.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirqualityhomeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
