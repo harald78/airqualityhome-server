31a1050c104aae6e2ef65bedd41172d7c06189b0
ski88 <>
2024-05-26 21:05:35 +0200

    feature/DWS-36: Test-Klassen angepasst

--- src/test/java/life/airqualityhome/server/rest/controller/NotificationsControllerTest.java
+++ src/test/java/life/airqualityhome/server/rest/controller/NotificationsControllerTest.java
@@ -1,28 +1,5 @@
 package life.airqualityhome.server.rest.controller;
 
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/notifications")
 public class NotificationsControllerTest {
 
-    @PostMapping("/read/{id}")
-    public String readNotification() {
-        return "Hello Notifications";
-    }
-
-    @GetMapping("/user/{id}")
-    public String getUserNotifications() {
-        return "Hello Notifications";
-    }
-
-    @DeleteMapping("/user/{id}")
-    public String deleteUserNotifications() {
-        return "Hello Notifications";
-    }
-
-    @DeleteMapping("/{id}")
-    public String deleteNotifications() {
-        return "Hello Notifications";
-    }
 }
