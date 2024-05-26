fff552962957d053a75b99d0ba43cb4ebe59433d
harald <harald@test.de>
2024-05-26 15:13:41 +0200

    DWS-36: Corrected dependency configuration in pom.xml. Changed SecurityConfig. Added /api/user controller for testing purposes

--- src/main/java/life/airqualityhome/server/config/SecurityConfig.java
+++ src/main/java/life/airqualityhome/server/config/SecurityConfig.java
@@ -6,47 +6,48 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    /*@Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http
-                //.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(new AntPathRequestMatcher("/swagger-ui/**")).permitAll()
-                        .requestMatchers("/api/**")
-                        .authenticated()
-                        .requestMatchers("/v3/**")
-                        .permitAll()
-                )
-                .httpBasic(withDefaults());
-        return http.build();
-    }*/
+    @Bean
+    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+        return new MvcRequestMatcher.Builder(introspector);
+    }
 
-    /*@Bean
+    @Bean
+    public SecurityFilterChain securityFilter(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests((exchange) -> exchange
+                        .requestMatchers(mvc.pattern("/swagger-resources"),
+                                mvc.pattern("/swagger-resources/**"),
+                                mvc.pattern("/webjars/**"),
+                                mvc.pattern("/v3/api-docs/**"),
+                                mvc.pattern("/swagger-ui/**"),
+                                mvc.pattern( "/swagger-ui.html"))
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
     public UserDetailsService userDetailsService() {
         var user = User.withUsername("user")
                 .password("{noop}s3cret!")
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(user);
-    }*/
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(request -> request.requestMatchers(new AntPathRequestMatcher("/api/**"))
-                .authenticated())
-                .httpBasic(Customizer.withDefaults())
-                .authorizeHttpRequests(request -> request.requestMatchers(new AntPathRequestMatcher("/swagger-ui**"))
-                .permitAll())
-                .build();
     }
 }
---
+++ src/main/java/life/airqualityhome/server/rest/controller/UserController.java
@@ -0,0 +1,15 @@
+package life.airqualityhome.server.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @GetMapping
+    public String getUser() {
+        return "Hello World";
+    }
+}
