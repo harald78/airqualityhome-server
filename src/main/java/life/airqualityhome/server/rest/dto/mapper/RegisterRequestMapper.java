22ef0f211d89ce69a7fdaffb84c5048645e457f4
Bjarne14 <bjarne.lehmann@student.wb-hochschule.com>
2024-05-28 22:26:19 +0200

    Update src/main/java/life/airqualityhome/server/rest/dto/mapper/RegisterRequestMapper.java
    Co-authored-by: harald78 <166989432+harald78@users.noreply.github.com>

--- src/main/java/life/airqualityhome/server/rest/dto/mapper/RegisterRequestMapper.java
+++ src/main/java/life/airqualityhome/server/rest/dto/mapper/RegisterRequestMapper.java
@@ -4,7 +4,7 @@ import life.airqualityhome.server.model.RegisterRequestEntity;
 import life.airqualityhome.server.rest.dto.RegisterRequestDto;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "string")
+@Mapper(componentModel = "spring")
 public interface RegisterRequestMapper {
     RegisterRequestDto toDto(RegisterRequestEntity registerRequest);
 }
